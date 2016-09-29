63a04d1af8afa256a17baf74ce129b2f8a8d0e17
pjrosand <pjr741@gmail.com>
2016-09-29 18:18:29 -0400

    Update BeerSong.java

--- BeerSong.java
+++ BeerSong.java
@@ -1,4 +1,7 @@
 public static void main(String args[]){
+                  	 Ninety_Nine_Bottles_of_Beer();	 
+}
+Ninety_Nine_Bottles_of_Beer()	 {
                         for(int i=99; i>0; i--){
               System.out.println(i + " bottles of beer on the wall " + i + " bottle of beer. Take one down pass it around " + (i-1) + " bottles of beer on the wall");
          }

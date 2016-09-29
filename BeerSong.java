19a2326ea16ed53fdd0e72b9e7f39cac18157f0b
pjrosand <pjr741@gmail.com>
2016-09-29 19:25:51 -0400

    Update BeerSong.java

--- BeerSong.java
+++ BeerSong.java
@@ -1,8 +1,11 @@
+Public class BeerSong implements implememntation{
 public static void main(String args[]){
+  
                   	 Ninety_Nine_Bottles_of_Beer();	 
 }
 Ninety_Nine_Bottles_of_Beer()	 {
                         for(int i=99; i>0; i--){
               System.out.println(i + " bottles of beer on the wall " + i + " bottle of beer. Take one down pass it around " + (i-1) + " bottles of beer on the wall");
          }
+}
 }

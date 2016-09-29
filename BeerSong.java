870de3454e7ec3addb4b1f2ecb0ba098047fe108
pjrosand <pjr741@gmail.com>
2016-09-29 18:01:37 -0400

    Create 	BeerSong.java

---
+++ BeerSong.java
@@ -0,0 +1,5 @@
+public static void main(String args[]){
+                        for(int i=99; i>0; i--){
+              System.out.println(i + " bottles of beer on the wall " + i + " bottle of beer. Take one down pass it around " + (i-1) + " bottles of beer on the wall");
+         }
+}
